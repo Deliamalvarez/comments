17d86a15dba740e980f06c001da9842c5785daa4
Delia Alvarez <deliamalvarez@gmail.com>
2018-04-30 06:40:48 -0300

    unit test in progress

--- comments/src/app/components/all-comments/all-comments.component.spec.ts
+++ comments/src/app/components/all-comments/all-comments.component.spec.ts
@@ -1,16 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Location, CommonModule } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpModule } from '@angular/http';
 
 import { AllCommentsComponent } from './all-comments.component';
+import { TruncatePipe } from '../../commons/length.pipe';
+import { CommentsService } from '../../services/comments.service';
 
 describe('AllCommentsComponent', () => {
   let component: AllCommentsComponent;
   let fixture: ComponentFixture<AllCommentsComponent>;
+  let commentService: CommentsService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AllCommentsComponent ]
+      declarations: [AllCommentsComponent, TruncatePipe],
+      imports: [RouterTestingModule, CommonModule, HttpModule],
+      providers: [CommentsService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- comments/src/app/services/comments.service.spec.ts
+++ comments/src/app/services/comments.service.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 
 import { CommentsService } from './comments.service';
 
 describe('CommentsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CommentsService]
+      providers: [CommentsService],
+      imports: [HttpModule]
     });
   });
 
