299fd65772dfcb4c3a47a6228488275a65ba3277
Delia <dalvarez@altimetrik.com>
2018-04-30 17:16:16 -0300

    unit test for components

--- comments/src/app/app.component.spec.ts
+++ comments/src/app/app.component.spec.ts
@@ -11,13 +11,17 @@ describe('AppComponent', () => {
       imports: [ RouterTestingModule ]
     }).compileComponents();
   }));
+  let fixture: any;
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+  
   it('should render router-outlet tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('router-outlet')).toBeDefined();
--- comments/src/app/components/all-comments/all-comments.component.spec.ts
+++ comments/src/app/components/all-comments/all-comments.component.spec.ts
@@ -1,3 +1,4 @@
+import { by } from 'protractor';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Location, CommonModule } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -6,17 +7,24 @@ import { HttpModule } from '@angular/http';
 import { AllCommentsComponent } from './all-comments.component';
 import { TruncatePipe } from '../../commons/length.pipe';
 import { CommentsService } from '../../services/comments.service';
+import { MOCK_COMMENTS } from '../../commons/mock.comments';
+import { Observable } from 'rxjs/Observable';
+import "rxjs/add/observable/of";
 
 describe('AllCommentsComponent', () => {
   let component: AllCommentsComponent;
   let fixture: ComponentFixture<AllCommentsComponent>;
-  let commentService: CommentsService;
+  let mockCommentService = {
+    getAllComments: () => {
+      return Observable.of(MOCK_COMMENTS);
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AllCommentsComponent, TruncatePipe],
       imports: [RouterTestingModule, CommonModule, HttpModule],
-      providers: [CommentsService]
+      providers: [{ provide: CommentsService, useValue: mockCommentService }]
     })
       .compileComponents();
   }));
@@ -29,5 +37,20 @@ describe('AllCommentsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have comments to load', () => {
+    expect(component.comments).toEqual(MOCK_COMMENTS);
+  });
+
+  it('should render title in h1 tag', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Comments summary list');
+  }));
+
+  it('should display all mock coments li items', function() {
+    const compiled = fixture.debugElement.nativeElement;
+    let liCount = compiled.querySelectorAll('li').length;
+    expect(liCount).toBe(MOCK_COMMENTS.length);
   });
 });
